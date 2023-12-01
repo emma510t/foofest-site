03ae05e69daffeb3f227f33062856e3679b9114e
Emil Kristensen <emil00y1@stud.kea.dk>
2023-12-01 14:29:04 +0100

    det virker ikke

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import { Header } from "rsuite";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import { RadioTile, RadioTileGroup } from "rsuite";
+import { Icon } from "@rsuite/icons";
+import { VscLock, VscWorkspaceTrusted, VscRepo } from "react-icons/vsc";
+
+import React from "react";
+
+function Header() {
+  return (
+    <RadioTileGroup defaultValue="private" aria-label="Visibility Level">
+      <RadioTile icon={<Icon as={VscLock} />} label="Private" value="private">
+        Project access must be granted explicitly to each user. If this project
+        is part of a group, access will be granted to members of the group.
+      </RadioTile>
+      <RadioTile
+        icon={<Icon as={VscWorkspaceTrusted} />}
+        label="Internal"
+        value="internal"
+      >
+        The project can be accessed by any logged in user except external users.
+      </RadioTile>
+
+      <RadioTile icon={<Icon as={VscRepo} />} label="Public" value="public">
+        The project can be accessed without any authentication.
+      </RadioTile>
+    </RadioTileGroup>
+  );
+}
+
+export default Header;
