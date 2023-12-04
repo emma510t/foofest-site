834d5beacaf22f6d025b4e3d4c3169dec226f046
Emma Hamdorf-Baunsgaard <emma510t@stud.kea.dk>
2023-12-04 12:58:05 +0100

    .

--- src/app/buy-tickets/page.jsx
+++ src/app/buy-tickets/page.jsx
@@ -1,3 +1,8 @@
+import "@shoelace-style/shoelace/dist/themes/light.css";
+import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path";
+
+setBasePath("https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.12.0/cdn/");
+
 import Footer from "@/components/Footer";
 import CampingAreas from "../layouts/CampingAreas";
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,8 @@
+import "@shoelace-style/shoelace/dist/themes/light.css";
+import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path";
+
+setBasePath("https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.12.0/cdn/");
+
 import Header from "@/components/Header";
 import Image from "next/image";
 
