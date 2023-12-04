83da0d8759884e96657a34d23cfea699702c949f
Emil Kristensen <emil00y1@stud.kea.dk>
2023-12-04 10:31:30 +0100

    fix

--- src/components/RadioTile.jsx
+++ src/components/RadioTile.jsx
@@ -1,5 +1,15 @@
+import styles from "@/modules/RadioTile.module.css";
+
 function RadioTile(data) {
-  return <input type="radio" className="flex flex-row-reverse border-2 rounded" value={data} name="campingarea" key={data.area}></input>;
+  return (
+    <input
+      type="radio"
+      className="flex flex-row-reverse border-2 rounded"
+      value={data}
+      name="campingarea"
+      key={data.area}
+    ></input>
+  );
 }
 
 export default RadioTile;
